47091e5db7cf4bf2430ecbb10f37520cf195b825
indirm <indirmuji@gmail.com>
2025-04-18 05:42:09 +0000

    s

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask, jsonify,  render_template
-
+from flask import Flask, jsonify, render_template, request, redirect, url_for, session
+from tinydb import TinyDB, Query
 
 app = Flask(__name__)
 
-assigned_ids = set()
+app.secret_key = "your_secret_key"
+
+
+COOK_USERNAME = "cook"
+COOK_PASSWORD = "1234"
+
+db = TinyDB("tables.json")
+Table = Query()
+
+if len(db) == 0:
+    for num in range(1, 31):
+        db.insert({"id": num, "is_taken": False})
+
 
 @app.route('/')
 def domov():
@@ -11,11 +23,21 @@ def domov():
 
 @app.route("/get_number", methods=["POST"])
 def get_number():
-    for num in range(1, 31):
-        if num not in assigned_ids:
-            assigned_ids.add(num)
-            return jsonify({"number": num})
-    return jsonify({"error": "Vse stevilke so zasedene"}), 400
+    free_table = db.get(Table.is_taken == False)
+
+    if free_table:
+        db.update({"is_taken": True}, Table.id == free_table["id"])
+        return jsonify({"number": free_table["id"]})
+    else:
+        return jsonify({"error": "Vse stevilke so zasedene"}), 400
+
+@app.route("/release_table/<int:table_id>", methods=["POST"])
+def release_table(table_id):
+    if db.contains(Table.id == table_id):
+        db.update({"is_taken": False}, Table.id == table_id)
+        return jsonify({"message": f"Miza {table_id} je zdaj sproščena."})
+    else:
+        return jsonify({"error": "Miza s tem ID-jem ne obstaja!"}), 404
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
